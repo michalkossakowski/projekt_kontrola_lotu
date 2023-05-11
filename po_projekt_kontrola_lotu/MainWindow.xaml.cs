9ef48b2b7c9f81f9d529ed18a62a3415dbbd67cb
michalkossakowski <kossakowskimichal2002@gmail.com>
2023-05-12 01:35:51 +0200

    nowy timer liczy czas i zmienia kolor

--- po_projekt_kontrola_lotu/MainWindow.xaml.cs
+++ po_projekt_kontrola_lotu/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 class Radar { }
 
@@ -22,9 +24,40 @@ namespace po_projekt_kontrola_lotu
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer _timer;
+        private int _counter = 0;
         public MainWindow()
         {
+            //poczatek main
+
             InitializeComponent();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += new EventHandler(dispatcherTimer_Tick);
+
+
+            //koniec main
         }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            _counter++;
+            TimerBox.Text = _counter.ToString();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+            Color customColor = Color.FromArgb(50, 0, 255, 0);
+            this.TimerBox.Background = new SolidColorBrush(customColor);
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            Color customColor = Color.FromArgb(50, 255, 0, 0);
+            this.TimerBox.Background = new SolidColorBrush(customColor);
+        }
+
     }
 }
