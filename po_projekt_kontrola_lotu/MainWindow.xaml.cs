45234ae9143498ce02710fa87cfa2570dce666d6
michalkossakowski <kossakowskimichal2002@gmail.com>
2023-05-12 20:13:42 +0200

    Dodane slidery do wybooru ilosci statkow oraz ktory statek ma miec zmienioną trase (liczba zmienia sie w zaleznosci od poprzedniego slidera) reset resetueje czas i slidery zanim nie ustawi sie ilosci statkow nie widac suwaka do naprawaiania trasy

--- po_projekt_kontrola_lotu/MainWindow.xaml.cs
+++ po_projekt_kontrola_lotu/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -33,8 +34,8 @@ namespace po_projekt_kontrola_lotu
         {
             //poczatek main
 
+            //timer
             InitializeComponent();
-
             _timer = new DispatcherTimer();
             _timer.Interval = TimeSpan.FromSeconds(1);
             _timer.Tick += new EventHandler(dispatcherTimer_Tick);
@@ -42,30 +43,67 @@ namespace po_projekt_kontrola_lotu
 
             //koniec main
         }
+
+        //timer
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
             _counter++;
             TimerBox.Text = _counter.ToString();
         }
-
-        private void btnStart_Click(object sender, RoutedEventArgs e)
+        private void Start_Click(object sender, RoutedEventArgs e)
         {
             _timer.Start();
-            Color customColor = Color.FromArgb(50, 0, 255, 0);
-            this.TimerBox.Background = new SolidColorBrush(customColor);
+            this.TimerBox.Background = new SolidColorBrush(Color.FromArgb(50, 0, 255, 0));
         }
-
-        private void btnStop_Click(object sender, RoutedEventArgs e)
+        private void Stop_Click(object sender, RoutedEventArgs e)
         {
             _timer.Stop();
-            Color customColor = Color.FromArgb(50, 255, 0, 0);
-            this.TimerBox.Background = new SolidColorBrush(customColor);
+            this.TimerBox.Background = new SolidColorBrush(Color.FromArgb(50, 255, 0, 0));
         }
 
-        private void Timer_Reset_Click(object sender, RoutedEventArgs e)
+
+        //reset
+        private void Reset_Click(object sender, RoutedEventArgs e)
         {
+            // timer
             _counter = 0;
             TimerBox.Text = _counter.ToString();
+            _timer.Stop();
+            this.TimerBox.Background = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+            // slidery
+            zmien_trase.Visibility = Visibility.Hidden;
+            slider2Text.Visibility = Visibility.Hidden;
+            wybierz_statek.Visibility = Visibility.Hidden;
+            slider2.Visibility = Visibility.Hidden;
         }
+
+
+
+        //slidery
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (slider1 != null && slider1Text != null)
+            {
+                slider1Text.Text = ((int)Math.Round(slider1.Value)).ToString();
+            }
+        }
+        private void slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (slider2 != null && slider2Text != null)
+            {
+                slider2Text.Text = ((int)Math.Round(slider2.Value)).ToString();
+            }
+        }
+        private void wygeneruj_Click(object sender, RoutedEventArgs e)
+        {
+            slider2.Maximum = ((int)Math.Round(slider1.Value));
+            zmien_trase.Visibility = Visibility.Visible;
+            slider2Text.Visibility = Visibility.Visible;
+            wybierz_statek.Visibility = Visibility.Visible;
+            slider2.Visibility = Visibility.Visible;
+        }
+
+
+
     }
 }
